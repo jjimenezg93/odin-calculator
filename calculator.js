f730e58665ed0fededca45a61d2195d98a1e4622
Julián Jiménez <jjimenezg93@gmail.com>
2025-07-05 10:30:14 +0100

    Add starting layout wihout implementation

---
+++ calculator.js
@@ -0,0 +1,22 @@
+let vals = [];
+let operator = 0;
+
+const add = function (a, b) {
+  return a + b;
+};
+
+const subtract = function (a, b) {
+  return a - b;
+};
+
+const multiply = function (a, b) {
+  return a * b;
+};
+
+const divide = function (a, b) {
+  return a / b;
+};
+
+const operate = function() {
+  
+}
